701ed8bcc7db243ecd3918b40089006fddd95eaa
jamesLaurino <petersthomas90@gmail.com>
2025-04-20 17:36:06 +0200

    Amélioration de la navigation dans la navigation.blade.php du dashboard

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -18,16 +18,7 @@
             </div>
             @else
                 <div class="my-2 my-lg-0 mr-5">
-                        @php
-                            $name = auth()->user()->name;
-                            $firstLetter = strtoupper(substr($name,0,1));
-                            $lastLetters = strtoupper(substr($name,-2));
-                            $color = substr(hash('sha256', $name),0,6);
-                        @endphp
-                         <a style="background-color: {{"#" . $color}}"
-                            class='nav-link text-dark rounded-circle' href='/dashboard'>
-                                    {{$firstLetter . $lastLetters}}
-                         </a>
+                    <x-trigram></x-trigram>
                 </div>
         @endif
     @endauth
