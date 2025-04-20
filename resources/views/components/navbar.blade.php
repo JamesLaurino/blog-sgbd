0e63edb9434df4459c9b6c4ff54887e7fe3c591a
jamesLaurino <petersthomas90@gmail.com>
2025-04-20 16:36:21 +0200

    Amélioration de l'avatar avec création d'un trigramme en runtime

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -9,18 +9,29 @@
         <span class="h3 mr-3">Mon blog</span>
     </div>
 
-    @if(auth()->user()->img_avatar)
-        <div class="my-2 my-lg-0 mr-5">
-            <a class="nav-link text-white" href="/dashboard">
-                <img width="50px" class="rounded-circle" height="50px" src="{{ asset('storage/uploads/' . auth()->user()->img_avatar) }}" />
-            </a>
-        </div>
-        @else
-        <div class="my-2 my-lg-0 mr-5">
-            <a class="nav-link text-white" href="/dashboard">Dashbord</a>
-        </div>
-    @endif
-
+    @auth
+        @if(auth()->user()->img_avatar)
+            <div class="my-2 my-lg-0 mr-5">
+                <a class="nav-link text-white" href="/dashboard">
+                    <img width="50px" class="rounded-circle" height="50px" src="{{ asset('storage/uploads/' . auth()->user()->img_avatar) }}" />
+                </a>
+            </div>
+            @else
+                <div class="my-2 my-lg-0 mr-5">
+                        @php
+                            $name = auth()->user()->name;
+                            $firstLetter = strtoupper(substr($name,0,1));
+                            $lastLetters = strtoupper(substr($name,-2));
+                            $white = "white";
+                            $color = substr(hash('sha256', $name),0,6);
+                        @endphp
+                         <a style="background-color: {{"#" . $color}}"
+                            class='nav-link text-dark rounded-circle' href='/dashboard'>
+                                    {{$firstLetter . $lastLetters}}
+                         </a>
+                </div>
+        @endif
+    @endauth
     @guest
         <div class="my-2 my-lg-0 mr-5">
             <a class="nav-link text-white" href="/login">connexion</a>
